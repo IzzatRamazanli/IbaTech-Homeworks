7a50ecc652080aadb069817eb7e5d96b5dd184d5
IzzatRamazanli <86556311+IzzatRamazanli@users.noreply.github.com>
2022-07-16 12:25:14 +0400

    Add files via upload

---
+++ homework4/src/main/java/org/example/Main.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Pet pet = new Pet();
+        pet.species = "dog";
+        pet.nickName = "Max";
+        pet.age = 2;
+        pet.trickLevel = 45;
+        pet.habits = new String[]{"eating", "sleeping", "playing with bone"};
+
+        Human father = new Human("John", "Maxwell", 1973);
+        Human mother = new Human("Jane", "Maxwell", 1977);
+
+        Human child = new Human("Mike", "Maxwell", 2002,
+                mother, father,
+                92,
+                pet,
+                new String[][]{{"monday, do exercise"}}
+        );
+
+
+        child.greetPet();
+        child.describePet();
+        pet.respond();
+
+        //Advanced part
+        //feeding pet
+        Random random = new Random();
+        int generatedNumber = random.nextInt(101);
+        boolean feedTime;
+        if (pet.trickLevel >= generatedNumber) {
+            feedTime = true;
+        } else {
+            feedTime = false;
+        }
+        child.feedPet(feedTime);
+        //End of advanced implementation
+
+        pet.eat();
+        System.out.println(child);
+
+
+    }
+}
