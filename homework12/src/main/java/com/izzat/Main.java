c0826170587b4970ba25605008fc9bf89b54bf9a
IzzatRamazanli <86556311+IzzatRamazanli@users.noreply.github.com>
2022-09-04 00:57:28 +0400

    hw12 updates

--- homework12/src/main/java/com/izzat/Main.java
+++ homework12/src/main/java/com/izzat/Main.java
@@ -1,17 +1,7 @@
 package com.izzat;
 
-import com.izzat.abstracts.Pet;
-import com.izzat.dao.FamilyController;
-import com.izzat.dao.FamilyDao;
-import com.izzat.dao.FamilyService;
-import com.izzat.dao.implementation.FamilyCollectionDao;
-import com.izzat.humans.Man;
-import com.izzat.humans.Woman;
-import com.izzat.pets.Dog;
-import com.izzat.pets.DomesticCat;
 
 import java.text.ParseException;
-import java.util.*;
 
 public class Main {
 
